ae96b36f0d06e01882d100a0e564dc4864f63a49
theapache64 <theapache64@gmail.com>
2020-03-27 02:03:30 +0530

    ◀️ confirm scholar response : backup

--- src/main/kotlin/com/teamxenox/codoc19/core/agents/TelegramBotAgent.kt
+++ src/main/kotlin/com/teamxenox/codoc19/core/agents/TelegramBotAgent.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.gson.Gson
 import com.teamxenox.codoc19.core.SecretConstants
 import com.teamxenox.codoc19.core.base.BotAgent
+import com.teamxenox.scholar.Scholar
+import com.teamxenox.scholar.subjects.corona.Corona
 import com.teamxenox.telegramapi.Telegram
 import com.teamxenox.telegramapi.models.SendMessageRequest
 import com.teamxenox.telegramapi.models.Update
@@ -25,6 +27,18 @@ class TelegramBotAgent : BotAgent {
         println("Message is `$message`")
         if (message == HELP_COMMAND || START_COMMAND == HELP_COMMAND) {
             sendHelp(update)
+        } else {
+            val answer = Scholar.getAnswer(Corona, message!!)
+            telegramApi.sendMessage(
+                    SendMessageRequest(
+                            update.message!!.chat!!.id!!,
+                            answer!!.answer,
+                            true,
+                            "HTML",
+                            update.message!!.messageId!!,
+                            null
+                    )
+            )
         }
     }
 
