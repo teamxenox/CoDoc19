ef33b3aeaa3bade311836d12b0947aa3b969ca62
theapache64 <theapache64@gmail.com>
2020-03-30 19:19:13 +0530

    ◀️ enhance country name matching

--- src/main/kotlin/com/teamxenox/codoc19/core/geography/Geographer.kt
+++ src/main/kotlin/com/teamxenox/codoc19/core/geography/Geographer.kt
@@ -17,7 +17,11 @@ object Geographer {
     fun getCountry(_text: String): Country? {
         val text = _text.toLowerCase().trim()
         for (country in countries) {
-            if (country.name.toLowerCase() == text || country.code.toLowerCase() == text) {
+            if (
+                    country.name.toLowerCase() == text ||
+                    country.code.toLowerCase() == text ||
+                    country.commonNames.find { it.toLowerCase() == text } != null
+            ) {
                 return country
             }
         }
