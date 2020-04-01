7004f9aa3d875d961c704a1b61d5e6536bee74e9
theapache64 <theapache64@gmail.com>
2020-04-01 20:26:46 +0530

    🆙 update timeseries data and cron_0530

--- cron_0530.sh
+++ cron_0530.sh
@@ -1,4 +1,4 @@
-# PROJECT_DIR="/home/theapache64/Documents/projects/codoc19"
+# PROJECT_DIR="/root/codoc19"
 PROJECT_DIR="/home/theapache64/Documents/projects/codoc19"
 RES_DIR="$PROJECT_DIR/covid19-api/src/main/resources"
 wget "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv" -O "$RES_DIR/time_series_covid19_confirmed_global.csv"
